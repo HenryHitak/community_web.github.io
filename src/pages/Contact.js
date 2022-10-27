c8aeb6b20d6f4ffb7b87c1f0add8a079ee781497
mamiko-s <mamiko.sugai@gmail.com>
2022-10-26 20:56:25 -0700

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,17 @@
 export default function Contact(){
     return(
         <>
-            <h1>Contact Page</h1>
+            <section className="contact">
+                <h1>Contact Us</h1>
+                <form>
+                    <input type="name" name="name" id="name" placeholder="Name"/>
+                    <input type="email" name="email" id="email" placeholder="Email"/>
+                    <textarea name="message" id="message" placeholder="Message"></textarea>
+                    <button type="submit" id="send">Send message</button>
+                </form>
+            </section>
+
+            
         </>
     )
 }
