68764e5656cdc3a5f398772bf1ba3fade8ac7b30
WunYu <Yu@Yu-MacBookPro.local>
2022-11-08 01:10:06 -0800

    add mail and hash

--- nodeDataBase/nodeServer.js
+++ nodeDataBase/nodeServer.js
@@ -1,4 +1,5 @@
 const formidable = require('formidable');
+const bcrypt = require('bcrypt');
 var http = require('http');
 var dataBase = require('./modules/dataBase');
 http.createServer((req,res)=>{
@@ -14,10 +15,20 @@ http.createServer((req,res)=>{
                     let loginCon = dataBase.dbConnect();
                     loginCon.connect((err)=>{
                         if(err) throw err;
-                        loginCon.query(dataBase.selectQuery('user_tb',`email = '${fields.user}' AND password = '${fields.pass}' `),(err,result)=>{
+                        loginCon.query(dataBase.selectQuery('user_tb',`email = '${fields.user}'`),(err,result)=>{
                             if(err) throw err;
                             if(result.length > 0){
-                                console.log(`${fields.user} çlogin success`);
+                                async function passVerify(password){
+                                    const chkPass = await bcrypt.compare(password,result[0].password);
+                                    if(chkPass){
+                                        console.log(`${fields.user} login success`);
+                                    }
+                                    else{
+                                        console.log('login failed');
+                                    }
+                                };
+                                passVerify(fields.pass);
+                                
                             }
                             else{
                                 console.log('login failed');
@@ -35,12 +46,16 @@ http.createServer((req,res)=>{
                     let insertCon = dataBase.dbConnect();
                     insertCon.connect((err)=>{
                         if(err) throw err;
-                        let insertQuery = `INSERT INTO user_tb (email,password,firstname,lastname,dob,gender) VALUES ('${fields.email}','${fields.password}','${fields.firstname}','${fields.lastname}','${fields.dob}','${fields.gender}')`;
-                        insertCon.query(insertQuery,(err,result)=>{
-                            if(err) throw err;
-                            res.write(result.affectedRows + 'Rows inserted');
-                            return res.end();
-                        })
+                        async function passHash(password) {
+                            const hash = await bcrypt.hash(password, 10);
+                            let insertQuery = `INSERT INTO user_tb (email,password,firstname,lastname,dob,gender) VALUES ('${fields.email}','${hash}','${fields.firstname}','${fields.lastname}','${fields.dob}','${fields.gender}')`;
+                            insertCon.query(insertQuery,(err,result)=>{
+                                if(err) throw err;
+                                res.write(result.affectedRows + 'Rows inserted');
+                                return res.end();
+                            })
+                          }  
+                        passHash(fields.password);
                     });
                 });
                 break;
