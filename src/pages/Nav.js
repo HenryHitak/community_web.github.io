bf8b16c2353546383b48131af5f7fa03295da3af
alycehwy <pray2451@gmail.com>
2023-01-01 23:20:32 -0800

    update sidebar

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,16 +1,18 @@
 import { Outlet, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SideBar from '../components/SideBar';
 import styles from '../css/Nav.module.css';
 import LoginModal from '../components/LoginModal';
 import RegModal from '../components/RegModal';
 
-export default function Nav(props){
+export default function Nav(){
     const [user,setUser] = useState([]);
     const [openLoginModal,setOpenLoginModal] = useState(false);
     const [openRegModal,setOpenRegModal] = useState(false);
     const [chkLoginFlag,setChkLoginFlag] = useState(false);
     const [chkAdminFlag,setChkAdminFlag] = useState(false);
+    const navigate = useNavigate();
     let chkStorageId = sessionStorage.getItem("sid");
     let chkStorageRole = sessionStorage.getItem("srole");
     function logout(){
@@ -18,6 +20,7 @@ export default function Nav(props){
         setChkAdminFlag(false);
         sessionStorage.removeItem('sid');
         sessionStorage.removeItem('srole');
+        navigate('/');
     };
     useEffect(()=>{
         if(chkStorageId != null){
@@ -27,17 +30,16 @@ export default function Nav(props){
             }
         }
     },[]);
-    
     return(
         <div>
             {openLoginModal ? <LoginModal closeModal={setOpenLoginModal} openReg={setOpenRegModal} loginFlag={setChkLoginFlag} adminFlag={setChkAdminFlag} loginFun={setUser}/> : null}
             {openRegModal ? <RegModal closeModal={setOpenRegModal}/> : null}
             <div className={styles.screen}>
-                <SideBar/>
+                <SideBar openLogin={setOpenLoginModal} loginFlag={chkLoginFlag} adminFlag={chkAdminFlag} logoutFlag={logout}/>
                 <section>
                     <header>
                         <div>
-                            {!chkLoginFlag ? <input type="button" onClick={()=>{setOpenLoginModal(true)}} value="Login"/> : <input type="button" onClick={()=>logout()}value="Logout"/>}
+                            {!chkLoginFlag ? <input type="button" onClick={()=>{setOpenLoginModal(true)}} value="Login"/> : <input type="button" onClick={()=>logout()} value="Logout"/>}
                             {!chkLoginFlag ? <input type="button" onClick={()=>{setOpenRegModal(true)}} value="Sign Up"/> : null}
                         </div>
                         <h1><Link to="/">Tian Tian</Link></h1>
