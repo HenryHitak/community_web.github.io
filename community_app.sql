d941f9a5ad43e975da44f5496fa98e9da0648e3b
ha-nakamura112 <nh.3213.b.b@gmail.com>
2023-01-11 06:53:25 +0900

    database updated

--- community_app.sql
+++ community_app.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- 생성 시간: 23-01-03 02:26
--- 서버 버전: 10.4.21-MariaDB
--- PHP 버전: 7.4.29
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2023 at 10:17 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.0.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 데이터베이스: `community_app`
+-- Database: `community_app`
 --
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `comment_tb`
+-- Table structure for table `comment_tb`
 --
 
 CREATE TABLE `comment_tb` (
@@ -38,7 +38,7 @@ CREATE TABLE `comment_tb` (
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `community_tb`
+-- Table structure for table `community_tb`
 --
 
 CREATE TABLE `community_tb` (
@@ -55,7 +55,7 @@ CREATE TABLE `community_tb` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 테이블의 덤프 데이터 `community_tb`
+-- Dumping data for table `community_tb`
 --
 
 INSERT INTO `community_tb` (`post_id`, `title`, `user_id`, `post_time`, `category`, `contents`, `img`, `location`, `price`, `view`) VALUES
@@ -64,7 +64,7 @@ INSERT INTO `community_tb` (`post_id`, `title`, `user_id`, `post_time`, `categor
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `job_tb`
+-- Table structure for table `job_tb`
 --
 
 CREATE TABLE `job_tb` (
@@ -83,7 +83,7 @@ CREATE TABLE `job_tb` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 테이블의 덤프 데이터 `job_tb`
+-- Dumping data for table `job_tb`
 --
 
 INSERT INTO `job_tb` (`post_id`, `title`, `user_id`, `post_time`, `company_name`, `location`, `salary`, `job_type`, `details`, `apply_type`, `benefits`, `view`) VALUES
@@ -95,7 +95,7 @@ INSERT INTO `job_tb` (`post_id`, `title`, `user_id`, `post_time`, `company_name`
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `market_td`
+-- Table structure for table `market_td`
 --
 
 CREATE TABLE `market_td` (
@@ -111,7 +111,7 @@ CREATE TABLE `market_td` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 테이블의 덤프 데이터 `market_td`
+-- Dumping data for table `market_td`
 --
 
 INSERT INTO `market_td` (`post_id`, `title`, `user_id`, `post_time`, `price`, `details`, `condition`, `location`, `view`) VALUES
@@ -123,7 +123,7 @@ INSERT INTO `market_td` (`post_id`, `title`, `user_id`, `post_time`, `price`, `d
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `user_tb`
+-- Table structure for table `user_tb`
 --
 
 CREATE TABLE `user_tb` (
@@ -137,34 +137,37 @@ CREATE TABLE `user_tb` (
   `img` varchar(200) DEFAULT './user_default.png',
   `role` varchar(250) NOT NULL DEFAULT 'user',
   `join_date` date DEFAULT NULL,
-  `status` varchar(100) NOT NULL DEFAULT 'activate'
+  `status` varchar(100) NOT NULL DEFAULT 'activate',
+  `link` varchar(100) DEFAULT NULL,
+  `exptime` int(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 테이블의 덤프 데이터 `user_tb`
+-- Dumping data for table `user_tb`
 --
 
-INSERT INTO `user_tb` (`user_id`, `email`, `password`, `firstname`, `lastname`, `dob`, `gender`, `img`, `role`, `join_date`, `status`) VALUES
-(2, 'alyce@tamwood.com', '1234', 'Alyce', 'Huang', '2022-10-24', 'F', NULL, 'user', NULL, 'Blocked'),
-(3, 'wunyu@tamwood.com', '1234', 'WunYu', 'Huang', '2022-10-25', 'N', NULL, 'user', NULL, 'active'),
-(6, 'test@tamwood.com', '1234', 'WunYu', 'Huang', '2022-10-24', 'N', NULL, 'user', NULL, 'active'),
-(10, 'wunyuaa@tamwood.com', '$2b$10$N2rHI8xItFgvEOMGvdSwCOk.B0rWI.GWz78mioYVrjAseI7cXGZly', 'Alyceee', 'Huanggg', '2022-11-01', 'F', './user_default.png', 'user', NULL, 'active'),
-(11, 'testtt@tamwood.com', '$2b$10$COx5FX1RIMaLH7Fy1nrSSeWAAAxoM1YcyAX0wiFxdRRxB.s72Wz1G', 'Test', 'Test', '2022-11-01', 'N', './user_default.png', 'user', NULL, 'active'),
-(12, 'nak@test.com', '$2b$10$hxlj6XbJSUYkHEcHwi/93e/mzI3oUbW2MwO./mceJxrsYtVlO05UO', 'Nakhyeon', 'kim', '2022-12-08', 'M', './user_default.png', 'admin', NULL, 'active'),
-(14, 'test1@tamwood.com', '$2b$10$DsDDWO5kFH8xcD6LGPgoAeqS5ZrX9ouioacYsMWdUjzMuXReed6ai', 'Alyce', 'HH', '2022-12-22', 'N', './user_default.png', 'admin', NULL, 'active'),
-(15, 'test2@tamwood.com', '$2b$10$8/Ze2PAk5t7KkkASbS4KSed4eOMvPTSd6IJf9aR/4vEkxC8a1SLEW', 'AA', 'HHH', '2022-12-22', 'F', './user_default.png', 'user', NULL, 'active'),
-(16, 'test3@tamwood.com', '$2b$10$EiDqgNLNqfT0HWNyBwt4JuCxEEG/.sUJ90.XAXBJCz8i47ZG/cqyi', 'WW', 'HH', '2022-12-22', 'M', './user_default.png', 'user', '2022-12-23', 'active'),
-(18, 'test4@tamwood.com', '$2b$10$pVhZccvpSoLkJuE4hFBxs.6g0rp1EIohdNDldMWt2kW8Fjcy.4vpO', 'Aly', 'Hua', '2022-12-25', 'F', './user_default.png', 'user', '2022-12-26', 'active'),
-(31, 'test6@tamwood.com', '$2b$10$7hORlpftoEHIDeWxBRYBJep0sSufdKjp/v1y/x1hBkGClemJyNh8i', 'AA', 'SS', '2022-12-27', 'N', './user_default.png', 'user', '2023-01-01', 'active'),
-(32, 'test7@tamwood.com', '$2b$10$BJUMxASIu74XULrCSrlMzu/sYYGP1vXHTx.MEpyDwcK4R6z6Q4yUm', 'AAA', 'SSS', '2023-01-01', 'F', './user_default.png', 'user', '2023-01-01', 'active'),
-(33, 'test8@tamwood.com', '$2b$10$TCekGHgO4L6Yuyr8eT0pS.uFiYj63G08.DSZOgArrqoCW5S7uoZT2', 'AAA', 'SS', '2022-12-27', 'N', './user_default.png', 'admin', '2023-01-01', 'active');
+INSERT INTO `user_tb` (`user_id`, `email`, `password`, `firstname`, `lastname`, `dob`, `gender`, `img`, `role`, `join_date`, `status`, `link`, `exptime`) VALUES
+(2, 'alyce@tamwood.com', '1234', 'Alyce', 'Huang', '2022-10-24', 'F', NULL, 'user', NULL, 'Blocked', NULL, NULL),
+(3, 'wunyu@tamwood.com', '1234', 'WunYu', 'Huang', '2022-10-25', 'N', NULL, 'user', NULL, 'active', NULL, NULL),
+(6, 'test@tamwood.com', '1234', 'WunYu', 'Huang', '2022-10-24', 'N', NULL, 'user', NULL, 'active', NULL, NULL),
+(10, 'wunyuaa@tamwood.com', '$2b$10$N2rHI8xItFgvEOMGvdSwCOk.B0rWI.GWz78mioYVrjAseI7cXGZly', 'Alyceee', 'Huanggg', '2022-11-01', 'F', './user_default.png', 'user', NULL, 'active', NULL, NULL),
+(11, 'testtt@tamwood.com', '$2b$10$COx5FX1RIMaLH7Fy1nrSSeWAAAxoM1YcyAX0wiFxdRRxB.s72Wz1G', 'Test', 'Test', '2022-11-01', 'N', './user_default.png', 'user', NULL, 'active', NULL, NULL),
+(12, 'nak@test.com', '$2b$10$hxlj6XbJSUYkHEcHwi/93e/mzI3oUbW2MwO./mceJxrsYtVlO05UO', 'Nakhyeon', 'kim', '2022-12-08', 'M', './user_default.png', 'admin', NULL, 'active', NULL, NULL),
+(14, 'test1@tamwood.com', '$2b$10$DsDDWO5kFH8xcD6LGPgoAeqS5ZrX9ouioacYsMWdUjzMuXReed6ai', 'Alyce', 'HH', '2022-12-22', 'N', './user_default.png', 'admin', NULL, 'active', NULL, NULL),
+(15, 'test2@tamwood.com', '$2b$10$8/Ze2PAk5t7KkkASbS4KSed4eOMvPTSd6IJf9aR/4vEkxC8a1SLEW', 'AA', 'HHH', '2022-12-22', 'F', './user_default.png', 'user', NULL, 'active', NULL, NULL),
+(16, 'test3@tamwood.com', '$2b$10$EiDqgNLNqfT0HWNyBwt4JuCxEEG/.sUJ90.XAXBJCz8i47ZG/cqyi', 'WW', 'HH', '2022-12-22', 'M', './user_default.png', 'user', '2022-12-23', 'active', NULL, NULL),
+(18, 'test4@tamwood.com', '$2b$10$pVhZccvpSoLkJuE4hFBxs.6g0rp1EIohdNDldMWt2kW8Fjcy.4vpO', 'Aly', 'Hua', '2022-12-25', 'F', './user_default.png', 'user', '2022-12-26', 'active', NULL, NULL),
+(31, 'test6@tamwood.com', '$2b$10$7hORlpftoEHIDeWxBRYBJep0sSufdKjp/v1y/x1hBkGClemJyNh8i', 'AA', 'SS', '2022-12-27', 'N', './user_default.png', 'user', '2023-01-01', 'active', NULL, NULL),
+(32, 'test7@tamwood.com', '$2b$10$BJUMxASIu74XULrCSrlMzu/sYYGP1vXHTx.MEpyDwcK4R6z6Q4yUm', 'AAA', 'SSS', '2023-01-01', 'F', './user_default.png', 'user', '2023-01-01', 'active', NULL, NULL),
+(33, 'test8@tamwood.com', '$2b$10$TCekGHgO4L6Yuyr8eT0pS.uFiYj63G08.DSZOgArrqoCW5S7uoZT2', 'AAA', 'SS', '2022-12-27', 'N', './user_default.png', 'admin', '2023-01-01', 'active', NULL, NULL),
+(34, 'bknb1102@icloud.com', '$2b$10$YZUgJXr07PaLtIs1u6Y5.uUrk9ChU9YlNcAJi9Q4VasLiA32TOzmu', 'haruka', 'nakamura', '0000-00-00', 'N', './user_default.png', 'user', NULL, 'activate', 'http://localhost:3000/reset?pass=$2b$10$YZUgJXr07PaLtIs1u6Y5.uUrk9ChU9YlNcAJi9Q4VasLiA32TOzmu', 1673384647);
 
 --
--- 덤프된 테이블의 인덱스
+-- Indexes for dumped tables
 --
 
 --
--- 테이블의 인덱스 `comment_tb`
+-- Indexes for table `comment_tb`
 --
 ALTER TABLE `comment_tb`
   ADD PRIMARY KEY (`comment_id`),
@@ -172,72 +175,72 @@ ALTER TABLE `comment_tb`
   ADD KEY `post_j` (`post_id`);
 
 --
--- 테이블의 인덱스 `community_tb`
+-- Indexes for table `community_tb`
 --
 ALTER TABLE `community_tb`
   ADD PRIMARY KEY (`post_id`),
   ADD KEY `post` (`user_id`);
 
 --
--- 테이블의 인덱스 `job_tb`
+-- Indexes for table `job_tb`
 --
 ALTER TABLE `job_tb`
   ADD PRIMARY KEY (`post_id`),
   ADD KEY `user` (`user_id`);
 
 --
--- 테이블의 인덱스 `market_td`
+-- Indexes for table `market_td`
 --
 ALTER TABLE `market_td`
   ADD PRIMARY KEY (`post_id`),
   ADD KEY `user_m` (`user_id`);
 
 --
--- 테이블의 인덱스 `user_tb`
+-- Indexes for table `user_tb`
 --
 ALTER TABLE `user_tb`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- 덤프된 테이블의 AUTO_INCREMENT
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- 테이블의 AUTO_INCREMENT `comment_tb`
+-- AUTO_INCREMENT for table `comment_tb`
 --
 ALTER TABLE `comment_tb`
   MODIFY `comment_id` int(250) NOT NULL AUTO_INCREMENT;
 
 --
--- 테이블의 AUTO_INCREMENT `community_tb`
+-- AUTO_INCREMENT for table `community_tb`
 --
 ALTER TABLE `community_tb`
   MODIFY `post_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- 테이블의 AUTO_INCREMENT `job_tb`
+-- AUTO_INCREMENT for table `job_tb`
 --
 ALTER TABLE `job_tb`
   MODIFY `post_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20005;
 
 --
--- 테이블의 AUTO_INCREMENT `market_td`
+-- AUTO_INCREMENT for table `market_td`
 --
 ALTER TABLE `market_td`
   MODIFY `post_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30005;
 
 --
--- 테이블의 AUTO_INCREMENT `user_tb`
+-- AUTO_INCREMENT for table `user_tb`
 --
 ALTER TABLE `user_tb`
-  MODIFY `user_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `user_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
--- 덤프된 테이블의 제약사항
+-- Constraints for dumped tables
 --
 
 --
--- 테이블의 제약사항 `comment_tb`
+-- Constraints for table `comment_tb`
 --
 ALTER TABLE `comment_tb`
   ADD CONSTRAINT `post_c` FOREIGN KEY (`post_id`) REFERENCES `community_tb` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -246,19 +249,19 @@ ALTER TABLE `comment_tb`
   ADD CONSTRAINT `user_c` FOREIGN KEY (`user_id`) REFERENCES `user_tb` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- 테이블의 제약사항 `community_tb`
+-- Constraints for table `community_tb`
 --
 ALTER TABLE `community_tb`
   ADD CONSTRAINT `post` FOREIGN KEY (`user_id`) REFERENCES `user_tb` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- 테이블의 제약사항 `job_tb`
+-- Constraints for table `job_tb`
 --
 ALTER TABLE `job_tb`
   ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user_tb` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- 테이블의 제약사항 `market_td`
+-- Constraints for table `market_td`
 --
 ALTER TABLE `market_td`
   ADD CONSTRAINT `user_m` FOREIGN KEY (`user_id`) REFERENCES `user_tb` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
